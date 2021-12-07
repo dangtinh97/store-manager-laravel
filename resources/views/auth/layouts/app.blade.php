48a11c6da6bf91c431631b1c253330fd62f19e12
Dang Tinh <>
2021-12-07 21:45:46 +0700

    dangtinh

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -19,11 +19,10 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    @php dd(env('APP_DEBUG'));@endphp
-    @if(!env('APP_DEBUG'))
-
+{{--    @php dd(env('APP_DEBUG'));@endphp--}}
+{{--    @if(!env('APP_DEBUG'))--}}
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    @endif
+{{--    @endif--}}
     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}">
     <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
     <title>

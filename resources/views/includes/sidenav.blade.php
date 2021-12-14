cb1c4bbd3f6e58efb3dce45fb9046b20d3f7b4ab
Dang Tinh <>
2021-12-15 01:10:36 +0700

    update

--- resources/views/includes/sidenav.blade.php
+++ resources/views/includes/sidenav.blade.php
@@ -1,3 +1,8 @@
+<style>
+    .navbar-vertical.navbar-expand-xs .navbar-collapse{
+        height: calc(100vh - 250px);
+    }
+</style>
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
@@ -76,6 +81,26 @@
                         <i class="material-icons opacity-10">person_add_alt</i>
                     </div>
                     <span class="nav-link-text ms-1">Tạo khách hàng</span>
+                </a>
+            </li>
+
+            <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Quản lý hợp đồng</h6>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="{{route('contracts.index')}}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">description</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Danh sách hợp đồng</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="{{route('contracts.create')}}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">add</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Tạo hợp đồng</span>
                 </a>
             </li>
 

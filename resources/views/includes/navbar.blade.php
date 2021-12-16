97bcbf44100050bc4ee810be35d1bdcadbfbb209
Dang Tinh <>
2021-12-17 03:58:15 +0700

    end

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
     <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-                <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Notifications</li>
-            </ol>
-            <h6 class="font-weight-bolder mb-0">Notifications</h6>
-        </nav>
+{{--        <nav aria-label="breadcrumb">--}}
+{{--            <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">--}}
+{{--                <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>--}}
+{{--                <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Notifications</li>--}}
+{{--            </ol>--}}
+{{--            <h6 class="font-weight-bolder mb-0">Notifications</h6>--}}
+{{--        </nav>--}}
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                 <div class="input-group input-group-outline">
@@ -43,10 +43,16 @@
                     </a>
                     <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropMenuAccount">
                         <li class="mb-2">
+                            <a class="dropdown-item border-radius-md" href="#">
+                                Thông tin tài khoản
+                            </a>
+                        </li>
+                        <li class="mb-2">
                             <a class="dropdown-item border-radius-md" href="{{route('logout')}}">
                                 Đăng xuất
                             </a>
                         </li>
+
                     </ul>
                 </li>
 

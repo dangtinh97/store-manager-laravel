e6d8f9912a5807c4db49190b58bf7cdfa687ab6c
Dang Tinh <>
2021-12-19 21:26:08 +0700

    update view

--- resources/views/includes/sidenav.blade.php
+++ resources/views/includes/sidenav.blade.php
@@ -60,14 +60,6 @@
                 </a>
             </li>
             @endcan
-            <li class="nav-item mt-3 cursor-pointer" onclick="addMenu();">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Quản lý hoạt động bán hàng
-                    <span class="material-icons" id="drop-button">
-arrow_drop_down
-</span>
-                </h6>
-            </li>
-            <ul class="navbar-nav ms-3" id="manager-nav-sell">
 
             <li class="nav-item mt-3">
                 <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Quản lý dự án</h6>
@@ -109,6 +101,14 @@ arrow_drop_down
                 </a>
             </li>
 
+            <li class="nav-item mt-3 cursor-pointer" onclick="addMenu();">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Quản lý hoạt động bán hàng
+                    <span class="material-icons" id="drop-button">
+arrow_drop_down
+</span>
+                </h6>
+            </li>
+            <ul class="navbar-nav ms-3" id="manager-nav-sell">
             <li class="nav-item mt-3">
                 <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Quản lý hợp đồng</h6>
             </li>
@@ -162,73 +162,6 @@ arrow_drop_down
                 </li>
             </ul>
 
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="pages/tables.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">table_view</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Tables</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="../pages/billing.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">receipt_long</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Billing</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="../pages/virtual-reality.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">view_in_ar</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Virtual Reality</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="../pages/rtl.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">format_textdirection_r_to_l</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">RTL</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white" href="../pages/notifications.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">notifications</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Notifications</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item mt-3">--}}
-{{--                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Account pages</h6>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="../pages/profile.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">person</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Profile</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="../pages/sign-in.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">login</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Sign In</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a class="nav-link text-white " href="../pages/sign-up.html">--}}
-{{--                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">--}}
-{{--                        <i class="material-icons opacity-10">assignment</i>--}}
-{{--                    </div>--}}
-{{--                    <span class="nav-link-text ms-1">Sign Up</span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
         </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
